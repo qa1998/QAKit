ac729ab4dc79e34439d219e2ea0707203c027852
Lê Quang Anh <anhlq@ftech.ai>
2023-07-10 10:06:40 +0700

    init example

---
+++ Example/QAKitExample/QAKitExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QAKitExample
+//
+//  Created by QuangAnh on 10/07/2023.
+//
+
+import UIKit
+import QAKit
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        QAKit.showLog()
+    }
+
+
+}
+
